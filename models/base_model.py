4683d18ab39bef3e5a31a615186146b5a2d8ab81
squidcarroll <squidcarroll@gmail.com>
2018-02-09 23:49:06 +0000

    udpate files

--- models/base_model.py
+++ models/base_model.py
@@ -4,5 +4,31 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """this is the base model"""
+    def __init__(self, *args, **kwargs):
+        ''' Initialize public instance attributes'''
+        if len(kwargs) is 0:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+
+
+    def __str__(self):
+        """return string in format"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                    self.id, self.__dict__)
+
+    def save(self):
+        """updates public instance with current time"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns dictionary of keys/values of instance"""
+        my_dict = dict(self.__dict__)
+        my_dict['__class__'] = self.__class__.__name__
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        return my_dict
