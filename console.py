873bb9a9c9fccbe8716937a0c47e84544868b531
squidcarroll <squidcarroll@gmail.com>
2018-02-13 22:14:38 +0000

    update console

--- console.py
+++ console.py
@@ -7,6 +7,11 @@ import cmd
 from models.base_model import BaseModel
 import models
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     """comand processor"""
