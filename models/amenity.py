869ed1f554ee5f6e4ee12827f276ca6fd1d6c71e
squidcarroll <squidcarroll@gmail.com>
2018-02-15 11:01:27 +0000

    update amenity pep8

--- models/amenity.py
+++ models/amenity.py
@@ -5,7 +5,7 @@ amenity class inherits from base class
 from models.base_model import BaseModel
 import models
 
- 
+
 class Amenity(BaseModel):
     """ amenity class inherits from base class """
     name = ""
